d7684745da56b817973c1217f83463535c4a81ea
andongni <x102039@gmail.com>
2023-09-15 14:02:23 +0800

    P2 00:00 / P3 (DONE)

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -3,15 +3,43 @@ using System;
 
 public partial class PlayerController : CharacterBody2D
 {
-    public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	private const float RUN_SPEED = 200f;
+	private const float JUMP_SPEED = 300f;
+	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	
+	// Components
+	Sprite2D _sprite => GetNode<Sprite2D>("Sprite2D");
+	AnimationPlayer _animationPlayer => GetNode<AnimationPlayer>("AnimationPlayer");
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector2 velocity = Velocity;
-        
-        velocity.Y += Gravity * (float)delta;
-        
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		// Input
+		float _direction = Input.GetAxis("move_left", "move_right");
+
+		// Run
+		Velocity = new Vector2(_direction * RUN_SPEED,
+			Velocity.Y + _gravity * (float)delta);
+
+		// Jump
+		if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+		{
+			Velocity = new Vector2(Velocity.X, -JUMP_SPEED);
+		}
+		
+		// Animation
+		if (IsOnFloor())
+		{
+			_animationPlayer.Play(_direction != 0 ? "running" : "idle");
+		}
+		else
+		{
+			_animationPlayer.Play("jump");
+		}
+		
+		// Flip sprite
+		if(_direction != 0) 
+			_sprite.FlipH = _direction < 0;
+
+		MoveAndSlide();
+	}
 }
