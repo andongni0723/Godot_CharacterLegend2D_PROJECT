a8046f3ec1c8c4c02852ebdf8d3083fada2740fd
andongni <x102039@gmail.com>
2023-09-24 08:59:26 +0800

    P5 DONE

--- scripts/Player/PlayerController.cs
+++ scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public enum State
 {
-	IDLE, RUNNING, JUMPING, FALLING
+	IDLE, RUNNING, JUMPING, FALLING, LANDING
 }
 
 public partial class PlayerController : CharacterController
@@ -13,9 +13,10 @@ public partial class PlayerController : CharacterController
 	private const float JUMP_SPEED = 350f;
 	private const float FLOOR_ACCELERATION = RUN_SPEED / 0.2f;
 	private const float AIR_ACCELERATION = RUN_SPEED / 0.02f;
-	private static readonly List<State> groundStates = new List<State> {State.IDLE, State.RUNNING};
-	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	private static readonly List<State> groundStates = new List<State> {State.IDLE, State.RUNNING, State.LANDING};
+	private float _defaultGravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	private bool _isFirstFrame = false; // if true, then jump velocity not change by gravity a frame
 	private bool _canJump = false;
 	private bool _isInputJump = false;
 	private bool _isOnFloorBeforeMove = false;
@@ -41,31 +42,41 @@ public partial class PlayerController : CharacterController
 			
 	}
 
+	/// <summary>
+	/// What do you do in this frame by State?
+	/// </summary>
+	/// <param name="state"></param>
+	/// <param name="delta"></param>
 	public override void TickPhysics(State state, double delta)
 	{
 		// switch enum State
 		switch (state)
 		{
 			case State.IDLE:
-				Move(delta);
+				Move(_defaultGravity, delta);
 				break;
 
 			case State.RUNNING:
-				Move(delta);
+				Move(_defaultGravity, delta);
 				break;
 
 			case State.JUMPING:
-				Move(delta);
+				Move(_isFirstFrame? 0 : _defaultGravity, delta);
 				break;
 
 			case State.FALLING:
-				Move(delta);
+				Move(_defaultGravity, delta);
+				break;
+			
+			case State.LANDING:
+				Move(_defaultGravity, delta);
 				break;
 		}
-		
+
+		_isFirstFrame = false;
 	}
 
-	private void Move(double delta)
+	private void Move(float gravity, double delta)
 	{
 		// Input
 		float _direction = Input.GetAxis("move_left", "move_right");
@@ -74,7 +85,7 @@ public partial class PlayerController : CharacterController
 		float _acceleration = IsOnFloor() ? FLOOR_ACCELERATION : AIR_ACCELERATION;
 		
 		Velocity = new Vector2(Mathf.MoveToward(Velocity.X, _direction * RUN_SPEED, _acceleration * (float)delta),
-			Velocity.Y + _gravity * (float)delta);
+			Velocity.Y + _defaultGravity * (float)delta);
 
 		// Flip sprite
 		if(_direction != 0) 
@@ -85,6 +96,11 @@ public partial class PlayerController : CharacterController
 	}
 
 
+	/// <summary>
+	/// Can to change State?
+	/// </summary>
+	/// <param name="state"></param>
+	/// <returns></returns>
 	public override State GetNextState(State state)
 	{
 		float direction = Input.GetAxis("move_left", "move_right");
@@ -122,13 +138,26 @@ public partial class PlayerController : CharacterController
 			
 			case State.FALLING:
 				if (IsOnFloor())
+				{
+					_animationPlayer.Stop();
+					return State.LANDING;	
+				}
+				break;
+			
+			case State.LANDING:
+				if (!_animationPlayer.IsPlaying())
 					return State.IDLE;
 				break;
 		}
 		
 		return state;
 	}
-
+	
+	/// <summary>
+	/// What do you do when you change State? (a frame)
+	/// </summary>
+	/// <param name="from"></param>
+	/// <param name="to"></param>
 	public override void TransitionToState(State from, State to)
 	{
 		if(!groundStates.Contains(from) && groundStates.Contains(to))
@@ -159,6 +188,12 @@ public partial class PlayerController : CharacterController
 					_coyoteTimer.Start();	
 				}
 				break;
+			
+			case State.LANDING:
+				_animationPlayer.Play("landing");
+				break;
 		}
+
+		_isFirstFrame = true;
 	}
 }
